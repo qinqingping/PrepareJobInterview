c29fe2aa77621c9bd52aac7e5804c9d819c66eab
qinqingping <1332981439@qq.com>
2018-07-30 18:53:51 +0800

    鍒濆鍖栭潰璇曢」鐩?

--- gitCommitCommand.bat
+++ gitCommitCommand.bat
@@ -1,7 +1,7 @@
 git config --global core.quotepath false          # 显示 status 编码
 git config --global gui.encoding utf-8            # 图形界面编码
 git config --global i18n.commit.encoding utf-8    # 提交信息编码
-git config --global i18n.logoutputencoding utf-8  # 输出 log 编码
+git config --global i18n.logoutputencoding urf-8  # 输出 log 编码
 git config --global user.name "qinqingping"
 git config --global user.email "1332981439@qq.com"
 git add -A
