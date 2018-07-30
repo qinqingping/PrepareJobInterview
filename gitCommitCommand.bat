74171090a491abc228a297c0b410273c212f1767
qinqingping <1332981439@qq.com>
2018-07-30 18:32:57 +0800

    初始化项目

---
+++ gitCommitCommand.bat
@@ -0,0 +1,10 @@
+git config --global core.quotepath false          # 显示 status 编码
+git config --global gui.encoding utf-8            # 图形界面编码
+git config --global i18n.commit.encoding utf-8    # 提交信息编码
+git config --global i18n.logoutputencoding utf-8  # 输出 log 编码
+git config --global user.name "qinqingping"
+git config --global user.email "1332981439@qq.com"
+git add -A
+git commit "初始化面试项目"
+git push
+git pull
